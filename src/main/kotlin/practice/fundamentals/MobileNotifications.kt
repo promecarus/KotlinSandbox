049bc99636dbcd838f4b3ca2760ab304827e9e2a
promecarus <haikalslipi@gmail.com>
2023-08-20 21:01:20 +0700

    Mobile notifications

---
+++ src/main/kotlin/practice/fundamentals/MobileNotifications.kt
@@ -0,0 +1,16 @@
+package practice.fundamentals
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    println(
+        if (numberOfMessages < 100) "You have $numberOfMessages notifications."
+        else "Your phone is blowing up! You have 99+ notifications."
+    )
+}
